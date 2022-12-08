b278284dc80ad028eb912e92ed678b0fb28299a7
Sean <seanboateng2@gmail.com>
2022-12-08 15:07:50 -0500

    Created Database + created local setting file + corrected database info

---
+++ musiclibrary_project/local_setting.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'music_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'flyhigh20'
+    }
+}
+
+SECRET_KEY = 'django-insecure-ij28()noys()8g$2cb6e2v(0-h6-r5g%k1$mc8i9m)bcra#gc*'
